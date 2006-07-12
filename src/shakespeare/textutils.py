d16b2ad323960c2a26ea1c42031fa0e9bb71f8a3
rgrp <rufus.pollock@okfn.org>
2006-07-12 08:37:16 +0000

    Update textutils.get_snippet to take fileobj rather than a textUrl (textUrls aren't being used anymore).   * this allows test to be refactored to use inline text

--- src/shakespeare/textutils.py
+++ src/shakespeare/textutils.py
@@ -1,9 +1,8 @@
 import shakespeare.utils
 
-def get_snippet(textUrl, charIndex, neighbourhood=30):
+def get_snippet(fileobj, charIndex, neighbourhood=30):
     # always get the cleaned version (since that is what concordancer uses
-    ff = file(shakespeare.utils.get_local_path(textUrl, 'cleaned'))
-    ss = ff.read()
+    ss = fileobj.read()
     start = max(0, charIndex - neighbourhood)
     # add some extra to take account that charIndex is from start of word
     extra = 8
--- src/shakespeare/textutils_test.py
+++ src/shakespeare/textutils_test.py
@@ -1,6 +1,8 @@
 import unittest
 
-import textutils
+import StringIO
+
+import shakespeare.textutils
 
 def test_suite():
     suites = [
@@ -11,12 +13,30 @@ def test_suite():
 class TextUtilsTest(unittest.TestCase):
     # ['The Phoenix and the Turtle',
     # 'http://www.gutenberg.org/dirs/etext98/cleaned2ws2710.txt', '']
-    textUrl = 'http://www.gutenberg.org/dirs/etext98/2ws2710.txt'
+    inStr = \
+'''THE PHOENIX AND THE TURTLE
+
+by William Shakespeare
+
+
+
+
+Let the bird of loudest lay,
+On the sole Arabian tree,
+Herald sad and trumpet be,
+To whose sound chaste wings obey.
+
+But thou, shrieking harbinger,
+Foul pre-currer of the fiend,
+Augur of the fever's end,
+To this troop come thou not near.'''
+
+    inFileObj = StringIO.StringIO(inStr)
     
     def test_get_snippet(self):
         exp = '''...Arabian tree,
 Herald sad and trumpet be,
 To whose sound chaste wing...'''
         # 125 is start of trumpet
-        out = textutils.get_snippet(self.textUrl, 125)
+        out = shakespeare.textutils.get_snippet(self.inFileObj, 125)
         self.assertEqual(exp, out)
