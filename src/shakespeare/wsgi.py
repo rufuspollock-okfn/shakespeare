fb311fa9f13884240892669e1ad58118abab701b
rgrp <rufus.pollock@okfn.org>
2007-04-07 13:13:18 +0000

    Remove cherrypy stuff (not used since move to plain wsgi and now getting out of date).

--- src/shakespeare/wsgi.py
+++ src/shakespeare/wsgi.py
@@ -1,17 +1,5 @@
 # wsgi applications interfaces
 
-def setup_cherrypy():
-    # this is for cherrypy 3.0
-    # for more information including how to set up fastcgi see
-    # http://www.cherrypy.org/wiki/FastCGIWSGI
-    import cherrypy
-    import shakespeare.cherrypy_handler 
-    app = cherrypy.tree.mount(shakespeare.cherrypy_handler.ShakespeareWebInterface())
-    # does not seem to be needed for wsgi stuff
-    # cherrypy.server.quickstart()
-    cherrypy.engine.start(blocking=False)
-    return app
-
 import shakespeare.wsgiplain
 
 def app_factory(global_config, **local_conf):
