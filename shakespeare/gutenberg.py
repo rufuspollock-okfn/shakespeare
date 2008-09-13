3df73679ec092f5efc52a5d53afbf904b53fa056
rgrp <rufus.pollock@okfn.org>
2008-09-13 13:39:19 +0000

    [shksprdata/getdata/gutenberg,shakespeare/gutenberg][m]: refactor gutenberg code to have common core in shakespeare and shakespeare-specific code in shksprdata/getdata/gutenberg.py.

--- shakespeare/gutenberg.py
+++ shakespeare/gutenberg.py
@@ -2,11 +2,9 @@
 """
 import os
 import StringIO
-import shakespeare.cache
 
-
-class GutenbergIndex(object):
-    """Parse the index of Gutenberg works so as to find Shakespeare works.
+class GutenbergIndexBase(object):
+    """Parse the index of Gutenberg works.
 
     TODO: Gutenberg now make available the index in RDF/XML form:
     http://www.gutenberg.org/feeds/catalog.rdf.bz2 and we should try to use
@@ -16,129 +14,34 @@ class GutenbergIndex(object):
     # url for the Gutenberg index file
     gutindex = 'http://www.gutenberg.org/dirs/GUTINDEX.ALL'
 
-    def __init__(self):
+    def __init__(self, cache):
+        self.cache = cache
         self.download_gutenberg_index()
-        self._gutindex_local_path = shakespeare.cache.default.path(self.gutindex)
+        self._gutindex_local_path = self.cache.path(self.gutindex)
 
     def download_gutenberg_index(self):
         """Download the Gutenberg Index file GUTINDEX.ALL to cache if we don't
         have it already.
         """
-        shakespeare.cache.default.download_url(self.gutindex)
+        self.cache.download_url(self.gutindex)
 
     def make_url(self, year, idStr):
         return 'http://www.gutenberg.org/dirs/etext%s/%s10.txt' % (year[2:], idStr)
 
-    def get_shakespeare_list(self):
-        """Get list of shakespeare works and urls.
-
-        Results are sorted by work title.
-
-        Notes regarding list of plays:
-
-          * no Folio edition of Troilus and Cressida
-          * no Folio edition of Pericles
-        """
-        # results have format [ title, url, comments ]
-        # folio in comments indicates it is a first folio
-        results = [ ["Sonnets", 'http://www.gutenberg.org/dirs/etext97/wssnt10.txt', ''] ]
-        plays = self._extract_shakespeare_works()
-        for play in plays:
-            url = self.make_url(play[1], play[2])
-            results.append([play[0], url, play[3]])
-        # add in by hand some exceptions
-        results.append(["The Winter's Tale",
-                'http://www.gutenberg.org/files/1539/1539.txt', '']
-                )
-        def compare_list(item1, item2):
-            if item1[0] > item2[0]: return 1
-            else: return -1
-        results.sort(compare_list)
-        return results
-    
-    def _extract_shakespeare_works(self):
-        """Get non-copyrighted Shakespeare works from Gutenberg
-        Results consist of folio and one other 'standard' version.
-        @return: list consisting of tuples in form [title, year, id, comment]
-        """
-        ff = file(self._gutindex_local_path)
-        results = []
-        for line in ff.readlines():
-            result = self.parse_line_for_folio(line)
-            if result:
-                results.append(result + ['folio'])
-            resultNormal = self.parse_line_for_normal(line)
-            if resultNormal:
-                results.append(resultNormal + [''])
-        return results
-    
-    def parse_line_for_normal(self, line):
-        """Parse GUTINDEX for 'normal' gutenberg shakespeare versions (i.e. not
-        folio and out of copyright).
-        """
-        # normal shakespeare are those with id starting [2
-        # most have 'by William Shakespeare' but also have 'by Shakespeare'
-        # (Othello) and 'by Wm Shakespeare' (Titus Andronicus)
-        # everything is by William Shakespeare except for Othello
-        if ('Shakespeare' in line and '[2' in line
-                and 'mp3' not in line and 'Apocrypha' not in line):
-            year = line[4:8]
-            tmp = line[9:]
-            endOfTitle = tmp.find(', by')
-            title = tmp[:endOfTitle]
-            startOfId = tmp.find('[2')
-            endOfId = tmp.find(']', startOfId)
-            idStr = tmp[startOfId+1:endOfId]
-            xstart = idStr.find('x')
-            idStr = idStr[:xstart]
-            return [title, year, idStr]
-        
-    def parse_line_for_folio(self, line):
-        if '[FF]' in line:
-            year = line[4:8]
-            tmp = line[9:]
-            endOfTitle = tmp.find(', by')
-            title = tmp[:endOfTitle]
-            startOfId = tmp.find('[FF]') + 5
-            endOfId = tmp.find(']', startOfId)
-            idStr = tmp[startOfId+1:endOfId]
-            xstart = idStr.find('x')
-            idStr = idStr[:xstart]
-            return [title, year, idStr]
-        else:
-            return None
-
-
-"""
-Clean up Gutenberg texts by removing all the header and footer bumpf
-"""
+    def get_relevant_works(self):
+        # implement in derived class 
+        raise NotImplementedError
 
 import re
-
 headerEndPhrases = ["Project Gutenberg's Etext of", 'This etext was prepared by']
 notesStartPhrases = ["Executive Director's Notes:"]
 notesEndPhrases = ['David Reed']
 footerStartPhrases = ['End of Project Gutenberg', 'End of The Project Gutenberg'
     ]
 
-def make_re_from_phrase(phrase):
+class GutenbergCleaner(object):
     """
-    Make a regular expression that matches a phrase and its surrounding
-    paragraph, i.e. that look like:
-    
-    ... phrase ....
-    more text
-    [blank]
-    [blank]+
-    """
-    paragraphText = '(.+\S.+\n)*' # need \S to ensure not just whitespace
-    # [[TODO: check slowdown due to inclusion of '^.*' at start
-    tmp = '^.*' + phrase + '.*\n' + paragraphText + '\s+'
-    return re.compile(tmp, re.I | re.M)  # make it case insensitive
-
-class GutenbergShakespeare(object):
-    """
-    Process Gutenberg shakespeare texts
+    Clean up Gutenberg texts by removing all the header and footer bumpf
     """
     
     def __init__(self, etext):
@@ -150,15 +53,31 @@ class GutenbergShakespeare(object):
             2. are there notes? If so strip them out
         """
         self.etext = etext
-        # most shakespeare texts are either ascii or latin-1
+        # most texts are either ascii or latin-1
         self.etextStr = unicode(self.etext.read(), 'latin-1').encode('utf-8')
         # normalize the line endings to save us grief later
         self.etextStr = self.etextStr.replace('\r\n', '\n')
         self.hasNotes = False
+
+    @classmethod
+    def make_re_from_phrase(self, phrase):
+        """
+        Make a regular expression that matches a phrase and its surrounding
+        paragraph, i.e. that look like:
+        
+        ... phrase ....
+        more text
+        [blank]
+        [blank]+
+        """
+        paragraphText = '(.+\S.+\n)*' # need \S to ensure not just whitespace
+        # [[TODO: check slowdown due to inclusion of '^.*' at start
+        tmp = '^.*' + phrase + '.*\n' + paragraphText + '\s+'
+        return re.compile(tmp, re.I | re.M)  # make it case insensitive
     
     def _find_max(self, phrase, string):
         maxIndex = 0
-        regex = make_re_from_phrase(phrase)
+        regex = self.make_re_from_phrase(phrase)
         matches = regex.finditer(string)
         for match in matches:
             maxIndex = max(match.end(), maxIndex)
@@ -166,7 +85,7 @@ class GutenbergShakespeare(object):
     
     def _find_min(self, phrase, string):
         minIndex = len(string)
-        regex = make_re_from_phrase(phrase)
+        regex = self.make_re_from_phrase(phrase)
         matches = regex.finditer(string)
         for match in matches:
             minIndex = min(match.start(), minIndex)
@@ -215,12 +134,15 @@ class GutenbergShakespeare(object):
 #    return ss
 
 
-class Helper(object):
+class HelperBase(object):
 
-    def __init__(self, verbose=False):
+    def __init__(self, works, cache, verbose=False):
+        '''
+        @param works: list of works (from e.g. GutenbergIndex.get_relevant_works)
+        '''
         self.verbose = verbose
-        gutindex = GutenbergIndex()
-        self._index = gutindex.get_shakespeare_list()
+        self.cache = cache
+        self._index = works
      
     def _filter_index(self, line):
         """Filter items in index return only those whose id (url) is in line.
@@ -241,22 +163,31 @@ class Helper(object):
         self.clean(line)
         self.add_to_db()
 
-    def get_index(self, line=None):
-        """Get list of texts
-        """
-        return self._index
-
     def download(self, line=None):
-        """Download from Project Gutenberg all the shakespeare texts listed in the
-        index.
+        """Download from Project Gutenberg all the texts wanted. 
         """
         for item in self._index:
             title = item[0]
             url = item[1]
             if self.verbose:
                 print 'Downloading %s (%s)' % (url, title)
-            shakespeare.cache.default.download_url(item[1])
+            self.cache.download_url(item[1])
     
+    def title_to_name(self, title):
+        """Convert a title to a unique name
+        """
+        tmp1 = title.replace(',', '')
+        tmp1 = tmp1.replace("'", '')
+        tmp1 = tmp1.lower()
+        # TODO: is 'king' too shakespeare specific ?
+        stripwords = [ 'king ', 'a ', 'the ' ]
+        for ww in stripwords:
+            if tmp1.startswith(ww):
+                tmp1 = tmp1[len(ww):]
+        tmp1 = tmp1.strip()
+        tmp1 = tmp1.replace(' ', '_')
+        return tmp1
+
     def clean(self, line=None):
         """Clean up raw gutenberg texts to extract underlying work (so remove
         all extra bumpf such as Gutenberg licence, contributor info etc).
@@ -266,69 +197,12 @@ class Helper(object):
         
         @param line: space separated list of text urls: text-url text-url
         """
-        textsToProcess = self._filter_index(line) 
-        for item in textsToProcess:
-            url = item[1]
-            src = shakespeare.cache.default.path(url)
-            dest = shakespeare.cache.default.path(url, 'plain')
-            if os.path.exists(dest):
-                if self.verbose:
-                    print 'Skip clean of %s as clean version already exists' % src
-                continue
-            if self.verbose:
-                print 'Formatting %s to %s' % (src, dest)
-            infile = file(src)
-            if src.endswith('wssnt10.txt'): # if it is the sonnets need a hack
-                # delete last 140 characters
-                tmp1 = infile.read()
-                infile = StringIO.StringIO(tmp1[:-120])
-            formatter = GutenbergShakespeare(infile)
-            ff = file(dest, 'w')
-            out = formatter.extract_text()
-            ff.write(out)
-            ff.close()
+        # implement in inheriting class
+        pass
 
-    def title_to_name(self, title):
-        """Convert a title to a unique name
-        """
-        tmp1 = title.replace(',', '')
-        tmp1 = tmp1.replace("'", '')
-        tmp1 = tmp1.lower()
-        stripwords = [ 'king ', 'a ', 'the ' ]
-        for ww in stripwords:
-            if tmp1.startswith(ww):
-                tmp1 = tmp1[len(ww):]
-        tmp1 = tmp1.strip()
-        tmp1 = tmp1.replace(' ', '_')
-        return tmp1
-
-    
     def add_to_db(self):
         """Add all gutenberg texts to the db list of texts.
         
-        If a text already exists in the db it will be skipped.
+        Stubbed: override in inheriting class.
         """
-        import shakespeare.model
-        for text in self._index:
-            title = text[0]
-            name = self.title_to_name(title) + '_gut'
-            url = text[1]
-            notes = 'Sourced from Project Gutenberg (url=%s). %s' % (text[1],
-                    text[2])
-            if text[2] == 'folio':
-                name += '_f'
-            
-            numExistingTexts = shakespeare.model.Material.query.filter_by(
-                        name=name).count()
-            if numExistingTexts > 0:
-                if self.verbose:
-                    print('Skip: Add to db. Gutenberg text already exists with name: %s' % name)
-            else:
-                if self.verbose:
-                    print('Add to db. Gutenberg text named [%s]' % name)
-                shakespeare.model.Material(name=name,
-                                        title=title,
-                                        creator='Shakespeare, William',
-                                        url=url,
-                                        notes=notes)
-
+        pass
--- shakespeare/tests/test_gutenberg.py
+++ shakespeare/tests/test_gutenberg.py
@@ -1,34 +1,9 @@
-import shakespeare.gutenberg
+import shakespeare.gutenberg as G
 import shakespeare.cache
-from shakespeare.gutenberg import make_re_from_phrase, GutenbergShakespeare 
 
 class TestGutenbergIndex:
 
-    gutindex = shakespeare.gutenberg.GutenbergIndex()
-    
-    def test_parse_line_for_folio(self):
-        inStr = 'Jul 2000 Cymbeline, by Wm. Shakespeare  [First Folio]=[FF] [0ws39xxx.xxx] 2269'
-        out = self.gutindex.parse_line_for_folio(inStr)
-        exp = ['Cymbeline', '2000', '0ws39']
-        for ii in range(len(exp)):
-            assert out[ii] == exp[ii]
-    
-    def test_parse_line_for_normal(self):
-        inStr = 'Nov 1998 Cymbeline, by William Shakespeare [2ws39xxx.xxx] 1538'
-        out = self.gutindex.parse_line_for_normal(inStr)
-        exp = ['Cymbeline', '1998', '2ws39']
-        for ii in range(len(exp)):
-            assert out[ii] == exp[ii]
-
-    def test_parse_line_for_normal_2(self):
-        "Added after discovering that Othello was not getting picked up."
-        inStr = 'Nov 1998 Othello, by Shakespeare [2ws32xxx.xxx] 1531'
-        out = self.gutindex.parse_line_for_normal(inStr)
-        print out
-        exp = ['Othello', '1998', '2ws32']
-        for ii in range(len(exp)):
-            assert out[ii] == exp[ii]
-
+    gutindex = G.GutenbergIndexBase(shakespeare.cache.default)
     
     def test_make_url(self):
         exp = 'http://www.gutenberg.org/dirs/etext00/0ws3910.txt'
@@ -40,11 +15,6 @@ class TestGutenbergIndex:
         out = self.gutindex.make_url('1998', '2ws39')
         assert exp == out
     
-    def test_get_shakespeare_list(self):
-        works = self.gutindex.get_shakespeare_list()
-        # figure derives from hand count in GUTINDEX.all
-        assert len(works) == 77
-
 
 class TestFormat:
     
@@ -54,7 +24,7 @@ class TestFormat:
             
             """
         inStr = outStr + 'All is Well that'
-        regex = make_re_from_phrase('blah')
+        regex = G.GutenbergCleaner.make_re_from_phrase('blah')
         out = regex.search(inStr)
         assert out.group() == outStr
     
@@ -66,11 +36,12 @@ class TestFormat:
             
             """
         inStr = outStr + 'All is Well that'
-        regex = make_re_from_phrase('blah')
+        regex = G.GutenbergCleaner.make_re_from_phrase('blah')
         out = regex.search(inStr)
         assert out.group() == outStr
 
-class TestGutenbergShakespeare:
+
+class TestGutenbergCleaner:
     # As you like it in Folio and normal
     url1 = 'http://www.gutenberg.org/dirs/etext00/0ws2510.txt'
     url2 = 'http://www.gutenberg.org/dirs/etext98/2ws2510.txt'
@@ -78,8 +49,8 @@ class TestGutenbergShakespeare:
     shakespeare.cache.default.download_url(url2)
     etext1 = file(shakespeare.cache.default.path(url1))
     etext2 = file(shakespeare.cache.default.path(url2))
-    gut1 = GutenbergShakespeare(etext1)
-    gut2 = GutenbergShakespeare(etext2)
+    gut1 = G.GutenbergCleaner(etext1)
+    gut2 = G.GutenbergCleaner(etext2)
     
     def test_get_header_end(self):
         out = self.gut1.get_header_end()
@@ -109,18 +80,10 @@ class TestGutenbergShakespeare:
             assert notFound
 
 import shakespeare.model
-class TestHelper:
+class TestHelperBase:
     url1 = 'http://www.gutenberg.org/dirs/etext00/0ws2510.txt'
     url2 = 'http://www.gutenberg.org/dirs/etext98/2ws2510.txt'
-    helper = shakespeare.gutenberg.Helper()
-
-    def test_clean(self):
-        line = '%s %s' % (self.url1, self.url2)
-        self.helper.clean(line)
-
-    def test_get_index(self):
-        out = self.helper.get_index()
-        assert 77 == len(out)
+    helper = G.HelperBase([], shakespeare.cache.default)
 
     def test_title_to_name(self):
         inlist = [ 'King Henry VIII', 
@@ -135,20 +98,4 @@ class TestHelper:
                     ]
         for ii in range(len(inlist)):
             assert explist[ii] == self.helper.title_to_name(inlist[ii])
-
-    def test_add_to_db(self):
-        self.helper.add_to_db()
-        text1 = shakespeare.model.Material.byName('hamlet_gut')
-        shakespeare.model.Material.byName('hamlet_gut_f')
-        assert 'Shakespeare, William' == text1.creator
-        alltexts = shakespeare.model.Material.query.all()
-        # do not delete because we may remove stuff that was there
-        # though this may undermine tests
-        # TODO: sort this out satisfactorily 
-        # for text in alltexts:
-        #    if '_gut' in text.name:
-        #        shakespeare.model.Material.delete(text.id)
-
-    def test_execute(self):
-        self.helper.execute()
 
