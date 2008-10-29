3261e472058e06cc17e73023e6ead0551e6de2c3
rgrp <rufus.pollock@okfn.org>
2008-10-29 20:35:17 +0000

    [l]: remove milton and milton_package as superseded by miltondata.

--- milton/textutils.py
+++
@@ -1,8 +0,0 @@
-def get_snippet(fileobj, charIndex, neighbourhood=30):
-    # always get the cleaned version (since that is what concordancer uses
-    ss = fileobj.read()
-    start = max(0, charIndex - neighbourhood)
-    # add some extra to take account that charIndex is from start of word
-    extra = 8
-    end = min(len(ss), charIndex + neighbourhood + extra)
-    return '...' + ss[start:end].strip() + '...'
