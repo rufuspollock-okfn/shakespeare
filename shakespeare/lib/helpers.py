d0b47527dc365753ffdefe7d14e547849d749c87
rgrp <rufus.pollock@okfn.org>
2009-06-15 07:58:41 +0000

    [pylons][xs]: fix up lib/helpers.py to use latest webhelpers (0.6.2) module.

--- shakespeare/lib/helpers.py
+++ shakespeare/lib/helpers.py
@@ -4,3 +4,8 @@ Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to both as 'h'.
 """
 from webhelpers import *
+try:
+    from webhelpers.rails.wrapped import *
+except:
+    pass
+from routes import url_for
