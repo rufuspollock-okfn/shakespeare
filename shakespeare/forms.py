00bba8876825188d05e2f879a85d4ca6aa3e02de
rgrp <rufus.pollock@okfn.org>
2009-10-24 15:18:48 +0000

    [forms][xs]: change textarea in work.

--- shakespeare/forms.py
+++ shakespeare/forms.py
@@ -3,7 +3,10 @@ from formalchemy import tables
 
 from shakespeare import model
 
-# Work = forms.FieldSet(model.Work)
+Work = forms.FieldSet(model.Work)
+Work.configure(options=[
+    Work.notes.textarea()
+    ])
 # Material = forms.FieldSet(model.Material)
 # Statistic = forms.FieldSet(model.Statistic)
 
