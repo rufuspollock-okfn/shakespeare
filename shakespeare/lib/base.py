9f8770f1e5f804734d23c947a4e2aa30538c4e01
rgrp <rufus.pollock@okfn.org>
2009-11-02 17:43:48 +0000

    [model][xs]: properly dispose of model.Session at end of use in controllers (we are getting errors about QueuePool limit from sqlalchemy).

--- shakespeare/lib/base.py
+++ shakespeare/lib/base.py
@@ -20,7 +20,10 @@ class BaseController(WSGIController):
         # WSGIController.__call__ dispatches to the Controller method
         # the request is routed to. This routing information is
         # available in environ['pylons.routes_dict']
-        return WSGIController.__call__(self, environ, start_response)
+        try:
+            return WSGIController.__call__(self, environ, start_response)
+        finally:
+            model.Session.remove()
 
     def __before__(self, action, **params):
         c.site_title = config.get('site_title', 'Open Shakespeare')
