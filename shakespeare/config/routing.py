0fa5b874052f7f34585e3f6f9371d02d7393746e
rgrp <rufus.pollock@okfn.org>
2010-02-26 18:31:30 +0000

    [config/routing.py][xs]: add /team/ url into site (about action).

--- shakespeare/config/routing.py
+++ shakespeare/config/routing.py
@@ -28,6 +28,7 @@ def make_map():
     map.connect('about', '/about/', controller='site', action='about')
     map.connect('news', '/news/', controller='site', action='news')
     map.connect('get-involved', '/get-involved/', controller='site', action='about')
+    map.connect('team', '/team/', controller='site', action='about')
     map.connect('wotw', '/wotw/{url:.*}', controller='site', action='wotw')
     map.connect('guide', '/guide/', controller='site', action='guide')
     map.connect('/resource/{action}/{id}{url:.*}', controller='our_resource')
