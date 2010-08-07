e14acab07d73e1b857a2a1cbd162d17126b45758
rgrp <rufus.pollock@okfn.org>
2010-08-07 19:54:23 +0100

    [controllers/template,cms][s+]: pull in sidebar from wordpress and faff around with css to get the two sites better integrated.

--- shakespeare/controllers/template.py
+++ shakespeare/controllers/template.py
@@ -35,6 +35,23 @@ class TemplateController(BaseController):
         else:
             return 'hello world'
             abort(404)
+
+    deliverance_rules = \
+'''<ruleset>
+  <theme href="%s" />
+  <!-- These are the default rules for anything with class="default" or no class: -->
+  <!-- suppress standard behaviour of copying over head stuff links, html, css
+  etc
+  <rule suppress-standard="1"> 
+  -->
+  <rule>
+      <replace content="children:/html/head/title" theme="children:/html/head/title" nocontent="ignore" />
+
+    <replace content="children:#content" theme="children:#content" />
+    <append content="children:#primary .xoxo" theme="children:#primary .xoxo" />
+  </rule>
+</ruleset>
+'''
     
     @property
     def deliverance(self):
@@ -43,7 +60,8 @@ class TemplateController(BaseController):
         proxy_base_url = config['deliverance.dest']
         theme_html = render('index.html')
         if not hasattr(self, '_deliverance'):
-            self._deliverance = create_deliverance_proxy(proxy_base_url, theme_html)
+            self._deliverance = create_deliverance_proxy(proxy_base_url,
+                    theme_html, rules_xml=self.deliverance_rules)
             # self._deliverance = create_deliverance_proxy()
         return self._deliverance
 
