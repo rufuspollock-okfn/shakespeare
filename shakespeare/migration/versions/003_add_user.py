fb2a607460147b151ec025fd1bafc8728c37900f
rgrp <rufus.pollock@okfn.org>
2011-01-20 02:15:07 +0000

    [migration][xs]: update existing migration to reflect new user table (bad to do this!).

--- shakespeare/migration/versions/003_add_user.py
+++ shakespeare/migration/versions/003_add_user.py
@@ -13,11 +13,13 @@ make_uuid = lambda: unicode(uuid.uuid4())
 user_table = Table('user', metadata,
         Column('id', UnicodeText, primary_key=True, default=make_uuid),
         Column('openid', UnicodeText, unique=True),
-        Column('nickname', UnicodeText, unique=True),
+        Column('username', UnicodeText, unique=True),
+        Column('fullname', UnicodeText),
         Column('email', UnicodeText, unique=True),
         Column('api_key', UnicodeText, default=make_uuid),
         Column('created', DateTime, default=datetime.now),
         Column('about', UnicodeText),
+        Column('meta', UnicodeText),
         )
 
 @wrap_in_transaction
