0d77877a0378755aaa352b5795eee368f0893682
rgrp <rufus.pollock@okfn.org>
2011-02-05 15:16:28 +0000

    [templates/widget,js][s+]: hack in a new widget to show latest annotations.
    * Very crude (so no filtering by service, text etc) but working!

---
+++ shakespeare/public/js/annotations.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+  var $mydiv = $('#latest-annotations');
+  var mytmpl = '<li><a href="/work/annotate/${uri}">${work}</a>: ${snippet}</li>';
+  var url = 'http://annotateit.org/store/annotations/search?limit=5&all_fields=1';
+  $.getJSON(url, function(data) {
+    $.each(data.rows, function(idx, row) {
+      var _t = row['text'];
+      row['snippet'] =  _t.length < 75 ? _t : _t.slice(0, 75) + '...';
+      var _uri = row['uri'];
+      row['work'] = _uri[0].toUpperCase() + _uri.slice(1).replace('_', ' ');
+    });
+    $mydiv.find('.total').html(data.total);
+    $mydiv.find('ul').html($.tmpl(mytmpl, data.rows));
+  });
+});
