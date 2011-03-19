7adf87a580a1142454681bf10466230db2c4c4b9
rgrp <rufus.pollock@okfn.org>
2011-03-19 14:44:33 +0000

    [js/annotations.js][s]: fix this up for new version of annotator schema and lib.

--- shakespeare/public/js/annotations.js
+++ shakespeare/public/js/annotations.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function($) {
   var $mydiv = $('#latest-annotations');
-  var mytmpl = '<li><a href="/work/annotate/${uri}">${work}</a>: ${snippet}</li>';
-  var url = 'http://annotateit.org/api/search?limit=5&all_fields=1';
+  var mytmpl = '<li><a href="${uri}">${work}</a>: ${snippet}</li>';
+  var url = 'http://annotateit.org/api/search?limit=5';
   $.getJSON(url, function(data) {
     $.each(data.rows, function(idx, row) {
       var _t = row['text'];
       row['snippet'] =  _t.length < 75 ? _t : _t.slice(0, 75) + '...';
-      var _uri = row['uri'];
-      row['work'] = _uri[0].toUpperCase() + _uri.slice(1).replace('_', ' ');
+      var _name = row['uri'].split('/');
+      var _name = _name[_name.length-1];
+      row['work'] = _name[0].toUpperCase() + _name.slice(1).replace(/_/g, ' ');
     });
     $mydiv.find('.total').html(data.total);
     $mydiv.find('ul').html($.tmpl(mytmpl, data.rows));
